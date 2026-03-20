6ae599f1e1b09c950eefaad96f30433be96389d6
Parvrajchauhan <parvrajchauhan@gmail.com>
2026-03-20 19:26:31 +0530

    cors issue fixed for production

--- Server/src/app.js
+++ Server/src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:8080','http://localhost:3000'],
+    origin: ['https://guid-1.onrender.com/login'],
     credentials: true,
 }));
 app.set("trust proxy", 1);
